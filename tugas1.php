57e6833135cf2e22a9dbbad7d3d7870d7635513c
reyy2212 <127268516+reyy2212@users.noreply.github.com>
2024-03-25 22:40:29 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,63 @@
+<html>
+     <head>
+        <title>Tugas1</title>
+     </head>
+
+     <body>
+
+     <a href="tugas11.php" target="_self"><input type="button" name="Ganti" value="Ganti"></a>
+    <table border="1">
+
+	<tr>
+	    <th rowspan="2">kode</th>
+        <th rowspan="2">nama</th>
+	    <th rowspan="2">satuan</th>
+	     <th colspan="2">harga</th>	   
+	    <th rowspan="2">Action</th>	
+	</tr>
+
+
+
+	<tr>             
+	    
+	     <td>beli</td>
+	     <td>jual</td>
+	
+
+	<tr>
+	     <td>M01</td>
+         <td>CPU</td>
+	     <td>PCS</td>
+	     <td>200</td>
+	     <td>350</td>
+	     <td>
+           
+		    <a href="tugas12.php" target="_self"><input type="button" name="update" value="update"></a>
+           
+	     </td>		
+	</tr>
+
+    <tr>
+	     <td>M02</td>
+         <td>RAM</td>
+	     <td>PCS</td>
+	     <td>300</td>
+	     <td>450</td>
+	     <td>
+         <a href="tugas12.php" target="_self"><input type="button" name="update" value="update"></a>
+	     </td>		
+	</tr>
+
+    <tr>
+	     <td>M03</td>
+         <td>VGA</td>
+	     <td>PCS</td>
+	     <td>400</td>
+	     <td>4550</td>
+	     <td>
+         <a href="tugas12.php" target="_self"><input type="button" name="update" value="update"></a>
+	     </td>		
+	</tr>
+    </table>
+     </body>
+</html>
---
+++ tugas12.php
@@ -0,0 +1,27 @@
+<html>
+     <head>
+        <title>Tugas1.2</title>
+     </head>
+
+     <body>
+
+        <form action="/action_page.php">
+            <label for="kode">Kode:</label><br>
+            <input type="text" id="kode" name="kode" value=""><br>
+            <label for="nama">nama:</label><br>
+            <input type="text" id="nama" name="nama" value=""><br><br>
+            <label for="satuan">Satuan:</label><br>
+            <input type="text" id="satuan" name="satuan" value=""><br><br>
+            <label for="Beli">Beli:</label><br>
+            <input type="text" id="Beli" name="Beli" value=""><br><br>
+            <label for="Jual">Jual:</label><br>
+            <input type="text" id="Jual" name="Jual" value=""><br><br>
+ 
+
+            <a href="tugas1.php" target="_self"><input type="button" value="Save"></a>
+            <a href="tugas1.php" target="_self"><input type="button" value="Delete"></a>
+            <a href="tugas1.php" target="_self"><input type="button" value="Cancel"></a>
+        </form> 
+
+    </body>
+</html>
